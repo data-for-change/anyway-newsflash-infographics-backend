98a3dd4846ecc9e14a7bd064729238a0032b1601
Daniel Shely <danielshely1@gmail.com>
2020-04-22 05:12:08 +0300

    caching infographic derived from  news flash req

--- routes/api.js
+++ routes/api.js
@@ -5,14 +5,17 @@ const NodeCache = require('node-cache');
 const ttl = 1000*60*60*24;
 const dataCache = new NodeCache();
 const apiConstants = require('../config/server-constants').api;
-const  getDataFromApi = (url) => {
-    return fetch(url)
-        .then((realServerRes)=>realServerRes.json()).catch(err=>console.log(err));
-};
+const  getDataFromApi = async  (url) => {
+    const res = await fetch(url);
+    const fetchedData = await res.json();
+    return fetchedData;
+}
+
 
 function getInfographicForNewsflash(id){
     const  urlAsKey = apiConstants.infographicsQuery + id;
-    fetch(urlAsKey).then(res=>res.json()).then(jsonObj=>dataCache.set(urlAsKey,jsonObj));
+    fetch(urlAsKey).then(res=>res.json()).catch(err=>console.log('error fetching url: ' + urlAsKey + ': ' + err)).
+    then(jsonObj=>dataCache.set(urlAsKey,jsonObj));
 }
 
 function setInfographicData(newsFlashIds){
@@ -21,30 +24,31 @@ newsFlashIds.forEach(newsId=>getInfographicForNewsflash(newsId));
 }
 setInterval(()=>dataCache.flushAll(),ttl);
 
- async function getData(urlAsKey,fetchData){
+    function getData(urlAsKey,fetchData){
     let dataFromCache;
     dataFromCache = dataCache.get(urlAsKey);
     if(!dataFromCache){
-        dataFromCache = await fetchData(urlAsKey);
+        dataFromCache =   fetchData(urlAsKey);
     }
     return dataFromCache;
 }
 
-router.get('/news-flash' ,async function (req,res){
+router.get('/news-flash' , async function (req,res){
     const url = `${apiConstants.baseUrl}${req.url}`;
-    const dateToSend = await  getData(url,(urlAsKey)=>{
-        const newFlashObjects =  getDataFromApi(urlAsKey,dataCache);
+        const dateToSend =  await  getData(url,   async(urlAsKey)=>{
+        const newFlashObjects =await    getDataFromApi(urlAsKey);
         dataCache.set(urlAsKey,newFlashObjects);
         const newsFlashIds = newFlashObjects.map(newsFlash=>newsFlash.id);
         setInfographicData(newsFlashIds,dataCache);
+        return newFlashObjects
     });
     res.send(dateToSend);
 });
 
 
-router.get('/infographics_data',async (req,res)=>{
+router.get('/infographics_data', async function (req,res) {
     const url = `${apiConstants.baseUrl}${req.url}`;
-    const dataToSend = await getData(url, (urlAsKey)=>{
+    const dataToSend =    await getData(url,  (urlAsKey)=>{
         const dataFromServer =  getDataFromApi(urlAsKey);
         dataCache.set(urlAsKey,dataFromServer);
         return dataFromServer;

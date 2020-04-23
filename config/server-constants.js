b03747452756fc1526d3869fa7c6418e124faa55
Daniel Shely <danielshely1@gmail.com>
2020-04-23 12:51:11 +0300

    added cahcing code in controller file and general refactoring

--- config/server-constants.js
+++ config/server-constants.js
@@ -1,6 +1,8 @@
 module.exports ={
     api:{
         baseUrl:'https://anyway.co.il/api',
-        infographicsQuery:'https://anyway.co.il/api/infographics_data?news_flash_id='
+        infographicsQuery:'https://anyway.co.il/api/infographics_data?news_flash_id=',
+        yearsAgo: '&years_ago=',
+        infographicTimeRange : [0,1, 3, 5, 8]
     }
 }
--- routes/api.js
+++ routes/api.js
@@ -1,60 +1,33 @@
 const express = require('express');
 const router = express.Router();
-const  fetch = require('node-fetch');
-const NodeCache = require('node-cache');
-const ttl = 1000*60*60*24;
-const dataCache = new NodeCache();
-const apiConstants = require('../config/server-constants').api;
-const  getDataFromApi = async  (url) => {
-    const res = await fetch(url);
-    const fetchedData = await res.json();
-    return fetchedData;
-}
+const controller = require('../controllers/apiController');
 
-
-function getInfographicForNewsflash(id){
-    const  urlAsKey = apiConstants.infographicsQuery + id;
-    fetch(urlAsKey).then(res=>res.json()).catch(err=>console.log('error fetching url: ' + urlAsKey + ': ' + err)).
-    then(jsonObj=>dataCache.set(urlAsKey,jsonObj));
-}
-
-function setInfographicData(newsFlashIds){
-newsFlashIds.forEach(newsId=>getInfographicForNewsflash(newsId));
-
-}
-setInterval(()=>dataCache.flushAll(),ttl);
-
-    function getData(urlAsKey,fetchData){
-    let dataFromCache;
-    dataFromCache = dataCache.get(urlAsKey);
-    if(!dataFromCache){
-        dataFromCache =   fetchData(urlAsKey);
+router.get('/news-flash', async function (req, res) {
+    let dateToSend = null;
+    try {
+        dateToSend = await controller.getData(req.url, 'newsFlash');
+    } catch (err) {
+        console.log(err)
     }
-    return dataFromCache;
-}
 
-router.get('/news-flash' , async function (req,res){
-    const url = `${apiConstants.baseUrl}${req.url}`;
-        const dateToSend =  await  getData(url,   async(urlAsKey)=>{
-        const newFlashObjects =await    getDataFromApi(urlAsKey);
-        dataCache.set(urlAsKey,newFlashObjects);
-        const newsFlashIds = newFlashObjects.map(newsFlash=>newsFlash.id);
-        setInfographicData(newsFlashIds,dataCache);
-        return newFlashObjects
-    });
     res.send(dateToSend);
 });
 
 
-router.get('/infographics_data', async function (req,res) {
-    const url = `${apiConstants.baseUrl}${req.url}`;
-    const dataToSend =    await getData(url,  (urlAsKey)=>{
-        const dataFromServer =  getDataFromApi(urlAsKey);
-        dataCache.set(urlAsKey,dataFromServer);
-        return dataFromServer;
-        });
+router.get('/infographics_data', async function (req, res) {
+    let dataToSend = null;
+    try {
+        dataToSend = await controller.getData(req.url, 'infographic');
+    } catch (err) {
+        console.log(err)
+    }
     res.send(dataToSend);
 });
 
+router.get('/clear-cache', (req, res) => {
+    controller.clearAPICache();
+    res.send('Cache Has Been Clear And Reset!');
+})
 
-module.exports= router;
+
+module.exports = router;

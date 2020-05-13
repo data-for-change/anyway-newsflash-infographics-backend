7d0dcea0fb0b3d6d6bd30d8ab7de19a96a5f24b3
Daniel Shely <danielshely1@gmail.com>
2020-05-13 23:35:25 +0300

    redirect to app url after and fixing debug issue with dote-fix

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router  = require('express').Router();
 const passport = require('passport');
+const {isProd} = require("../config/passport-setup");
 
 router.get('/google-login/success', (req, res) => {
         if (req.user) {
@@ -24,10 +25,9 @@ router.get('/google-login',passport.authenticate('google', {
     scope:['profile'],
 }));
 
-router. get('/google-login/redirect', passport.authenticate('google',{
-    successRedirect: process.env.APP_URL,
-    failureRedirect:'/'
-}));
+router. get('/google-login/redirect', passport.authenticate('google'),(req,res)=>{
+    res.redirect(req.get('referrer'));
+})
 
 router.get('/logout',(req,res)=> {
     req.logOut();

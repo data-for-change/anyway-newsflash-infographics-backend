beaf8920d34c4588da73d86fc42267e6f5099eb3
yuvalbl <yuval.bar.levi@gmail.com>
2020-06-17 17:45:55 +0300

    update baseUrl

--- config/server-constants.js
+++ config/server-constants.js
@@ -1,7 +1,7 @@
 
 module.exports ={
     api:{
-        baseUrl:'https://anyway.co.il/api',
+        baseUrl:'https://www.anyway.co.il/api',
         infographicsQuery:'https://anyway.co.il/api/infographics_data?news_flash_id=',
         yearsAgo: '&years_ago=',
         infographicTimeRange : [0,1, 3, 5, 8]
